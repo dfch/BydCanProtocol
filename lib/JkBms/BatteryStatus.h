c5e4bfb4838f90fa5187386a162394bd0d44670a
Ronald Rink <ronald.rink@d-fens.net>
2023-08-30 22:41:49 +0200

    Status information
     * misc enums from proto description

---
+++ lib/JkBms/BatteryStatus.h
@@ -0,0 +1,26 @@
+/**
+* Copyright (c) Ronald Rink
+* See LICENSE file in the project root for full license information.
+*/
+
+#pragma once
+
+#include <cstdint>
+
+namespace JkBms
+{
+    enum class BatteryStatus : uint8_t
+    {
+        ChargingOn = 1 << 0,   // 0th bit: Charging MOSFET state is ON
+        ChargingOff = 0 << 0,  // 0th bit: Charging MOSFET state is OFF
+
+        DischargingOn = 1 << 1,   // 1st bit: Discharging MOSFET state is ON
+        DischargingOff = 0 << 1,  // 1st bit: Discharging MOSFET state is OFF
+
+        BalancingOn = 1 << 2,   // 2nd bit: Balancing switch state is ON
+        BalancingOff = 0 << 2,  // 2nd bit: Balancing switch state is OFF
+
+        BatteryConnected = 1 << 3,   // 3rd bit: Battery is connected
+        BatteryDisconnected = 0 << 3 // 3rd bit: Battery is disconnected
+    };
+}
---
+++ lib/JkBms/StatusFlags.h
@@ -0,0 +1,32 @@
+/**
+* Copyright (c) Ronald Rink
+* See LICENSE file in the project root for full license information.
+*/
+
+#pragma once
+
+#include <cstdint>
+
+namespace JkBms
+{
+    enum class StatusFlags : uint16_t 
+    {
+        LowCapacityAlarm = 0x0001,
+        MOSFETOverTemp = 0x0002,
+        ChargeOverVoltage = 0x0004,
+        DischargeUnderVoltage = 0x0008,
+        BatteryOverTemp = 0x0010,
+        ChargeOverCurrent = 0x0020,
+        DischargeOverCurrent = 0x0040,
+        CellVoltageDifference = 0x0080,
+        InternalOverTemp = 0x0100,
+        LowTempAlarm = 0x0200,
+        SingleCellOverVoltage = 0x0400,
+        SingleCellUnderVoltage = 0x0800,
+        Protect309_A = 0x1000,
+        Protect309_B = 0x2000,
+        Reserved1 = 0x4000,
+        Reserved2 = 0x8000
+    };
+
+}
