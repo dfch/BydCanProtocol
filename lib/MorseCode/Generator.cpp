fc4c623dca9e8e2402858fe169f1903a4ba92449
Ronald Rink <ronald.rink@d-fens.net>
2023-09-04 12:57:21 +0200

    license header update
     * replaced header by expliciting mentioning MIT
     * replaced comment style

--- lib/MorseCode/Generator.cpp
+++ lib/MorseCode/Generator.cpp
@@ -1,7 +1,6 @@
-/**
-* Copyright (c) Ronald Rink
-* See LICENSE file in the project root for full license information.
-*/
+// Copyright (c) 2023 Ronald Rink
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// SPDX-License-Identifier: MIT
 
 #include "Generator.h"
 
